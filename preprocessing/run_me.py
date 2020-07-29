5861f52b84483f3add28f638d9a969352a6527cb
Linta Oommen <lintaoommen@localhost.localdomain>
2020-07-29 23:25:43 +0530

    Adding images

--- preprocessing/run_me.py
+++
@@ -1,9 +0,0 @@
-import preprocess
-
-input_image = Preprocess()
-
-#Call preprocess functions
-input_image.skull_strip(src_path,dst_path)
-input_image.bias_correction(dst_path,dst_path_1)
-input_image.segmentation(dst_path_1,dst_path_2)
-input_image.final_two_dimensional_image(dst_path_2,final_dst_path)
